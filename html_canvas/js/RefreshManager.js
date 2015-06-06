0605d0ea9192ae12d31df69f061d72601dd06922
yubaoquan <1079139460@qq.com>
2015-06-06 16:34:54 +0800

    add css
    I don't like css.

--- html_canvas/js/RefreshManager.js
+++ html_canvas/js/RefreshManager.js
@@ -3,16 +3,31 @@ function RefreshManager() {
 }
 
 RefreshManager.prototype = {
-	thingsToDraw: [],
-	refresh: function() {
-		window.pen.clearRect(0, 0, Config.canvasWidth, Config.canvasHeight);
-		for (var i = 0, len = this.thingsToDraw.length; i < len; i ++) {
-			// console.info(this.thingsToDraw[i]);
-			this.thingsToDraw[i].draw();
-		}
-	},
-	add: function(thing) {
-		this.thingsToDraw.push(thing);
-		return this;
-	}
+    thingsToDraw: [],
+    infoPanel: document.querySelector('.status-content'),
+    infoRows: 0,
+    printInfo: function(info) {
+        if (this.infoRows >= 50) {
+        	this.infoPanel.innerHTML = '';
+        	this.infoRows = 0;
+        }
+        this.infoPanel.innerHTML += new Date().toLocaleTimeString() + ':<br>';
+        this.infoPanel.innerHTML += info + '<br>';
+        this.infoPanel.scrollTop = this.infoPanel.scrollHeight;
+        this.infoRows ++;
+    },
+    refresh: function() {
+        for (var i = 0; i < 100; i++) {
+            this.printInfo('xxxxxx');
+        }
+        window.pen.clearRect(0, 0, Config.canvasWidth, Config.canvasHeight);
+        for (var i = 0, len = this.thingsToDraw.length; i < len; i++) {
+            // console.info(this.thingsToDraw[i]);
+            this.thingsToDraw[i].draw();
+        }
+    },
+    add: function(thing) {
+        this.thingsToDraw.push(thing);
+        return this;
+    }
 };
